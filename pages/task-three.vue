0927882e449d0e1fdb9dbca7cfbbf154f08e05be
viankathdev <vian@nathanhr.com>
2022-02-02 14:41:09 +0800

    Revert "Revert "Initial commit""
    This reverts commit c94cb5ddda837975fd9cd2d3f7443d13e6733d56.

---
+++ pages/task-three.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row>
+    <v-col>
+        <img
+            src="/v.png"
+            alt="Vuetify.js"
+            class="mb-5"
+        >
+        <h2>Task #3 | Create Pages</h2>
+        <br>
+        <p>Create 5 pages under the page folder. See pages below. </p>
+        <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/task-one">Task One</NuxtLink></li>
+            <li><NuxtLink to="/task-two">Task Two</NuxtLink></li>
+            <li><NuxtLink to="/task-three">Task Three</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+        <br>
+        <v-btn
+            class="ma-2"
+            outlined
+            color="gray"
+            nuxt
+            to="/"
+        >
+        Back
+        </v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'TaskThreePage'
+}
+</script>
---
+++ pages/task.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-row>
+    <v-col>
+      <img
+        src="/v.png"
+        alt="Vuetify.js"
+        class="mb-5"
+      >
+      <h2>Tasks</h2>
+      <br>
+      <v-simple-table>
+        <template v-slot:default>
+        <thead>
+            <tr>
+            <th class="text-left">
+                Tasks
+            </th>
+            <th class="text-left">
+                Status
+            </th>
+             <th class="text-left">
+                Action
+            </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Task #1</td>
+                <td>
+                    <v-btn
+                    class="ma-2"
+                    text
+                    icon
+                    color="green lighten-2"
+                    >
+                    <v-icon>mdi-check-circle-outline</v-icon>
+                </v-btn>
+                </td>
+                <td>
+                    <v-btn
+                    color="primary"
+                    nuxt
+                    outlined
+                    to="/task-one"
+                    >
+                    <v-icon left>
+                        mdi-eye
+                    </v-icon>
+                    View
+                    </v-btn>
+                </td>
+            </tr>
+            <tr>
+                <td>Task #2</td>
+                <td>
+                        <v-btn
+                        class="ma-2"
+                        text
+                        icon
+                        color="green lighten-2"
+                        >
+                        <v-icon>mdi-check-circle-outline</v-icon>
+                    </v-btn>
+                </td>
+                <td>
+                    <v-btn
+                    color="primary"
+                    outlined
+                    nuxt
+                    to="/task-two"
+                    >
+                    <v-icon left>
+                        mdi-eye
+                    </v-icon>
+                    View
+                    </v-btn>
+                </td>
+            </tr>
+            <tr>
+                <td>Task #3</td>
+                <td>
+                        <v-btn
+                        class="ma-2"
+                        text
+                        icon
+                        color="green lighten-2"
+                        >
+                        <v-icon>mdi-check-circle-outline</v-icon>
+                    </v-btn>
+                </td>
+                <td>
+                    <v-btn
+                    color="primary"
+                    outlined
+                    nuxt
+                    to="/task-three"
+                    >
+                    <v-icon left>
+                        mdi-eye
+                    </v-icon>
+                    View
+                    </v-btn>
+                </td>
+            </tr>
+        </tbody>
+        </template>
+    </v-simple-table>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'TaskPage'
+}
+</script>
