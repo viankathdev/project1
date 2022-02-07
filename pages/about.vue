95f9f66ab078482f09e0719e178369affef1c410
viankathdev <vian@nathanhr.com>
2022-02-07 10:23:22 +0800

    -revamp about layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,20 +1,14 @@
 <template>
-  <v-row>
-    <v-col class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        &#8220;Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quaerat temporibus pariatur? Quam eos, alias rem sapiente temporibus tenetur et velit itaque consectetur, a minus voluptatem, veritatis nostrum esse accusamus!&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+  <v-row class="mt-16">
+    <v-col class="text-center mt-5">
+      <h1 class="text-uppercase text-h5 font-weight-bold mb-5 text-center"> Building the future we want to work in </h1>
+      <p class="text-body-1 mx-auto grey--text text--darken-1 text-center mb-10" style="max-width: 750px;"> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, in! Asperiores, impedit libero. 
+        Veniam rerum saepe unde nihil possimus quibusdam esse accusamus mollitia magni fuga. 
+      </p>
     </v-col>
+    <!-- Insert Feature's section -->
+    <AboutFeature />
   </v-row>
 </template>
 
