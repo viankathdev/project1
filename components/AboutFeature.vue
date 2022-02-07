06d482457cdb6a409c2a1949033d472d06d232a6
viankathdev <vian@nathanhr.com>
2022-02-07 10:21:10 +0800

    -created featured card component

---
+++ components/AboutFeature.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <div class="d-flex d-inline-flex align-center">
+      <v-col class="col-md-6 col-12">
+          <div>
+              <h2 class="mb-3" style="font-size: 3em;">A task manager you can trust for life</h2>
+              <p style="max-width: 500px;">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eum libero similique fuga quae nesciunt ea natus delectus dolorem molestias perferendis quas ducimus eos, debitis explicabo nam, eius repudiandae nemo.
+              </p>
+          </div>
+      </v-col>
+      <v-col class="col-md-6 col-12">
+          <img
+          width="80%"
+          src="/about-ft-1.png"
+      >
+      </v-col>
+    </div>
+    <div class="d-flex d-inline-flex align-center mt-10 p-10">
+      <v-col class="col-md-6 col-12">
+          <img
+          width="80%"
+          src="/about-ft-img-2.png"
+      >
+      </v-col>
+      <v-col class="col-md-6 col-12">
+          <div>
+              <h2 class="mb-3" style="font-size: 3em;">Everywhere you are</h2>
+              <p style="max-width: 500px;">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia eum libero similique fuga quae nesciunt ea natus delectus dolorem molestias perferendis quas ducimus eos, debitis explicabo nam, eius repudiandae nemo.
+              </p>
+          </div>
+      </v-col>
+    </div>
+  </v-row>
+</template>
